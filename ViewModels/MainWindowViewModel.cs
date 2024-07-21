071cd90b3766fa5439d43738858fc8d7de1c0e02
Daniel Skýpala <daniel@honza.info>
2024-07-21 10:49:53 +0200

    Implement visual mode

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,14 @@
 using ReactiveUI;
 using ChronoGit.Models;
 using System.Collections.ObjectModel;
+using System;
 
 namespace ChronoGit.ViewModels;
+
+public enum Mode {
+    NormalMode,
+    VisualMode
+};
 
 public class MainWindowViewModel : ViewModelBase {
     private ObservableCollection<CommandViewModel> _actions;
@@ -20,21 +26,40 @@ public class MainWindowViewModel : ViewModelBase {
         _actions[0].Selected = true;
     }
 
-    public int Selected = 0;
+    public Mode CurrentMode { get; private set; } = Mode.NormalMode;
+    public int CurrentPosition { get; private set; } = 0;
+    public int VisualModeStartPosition { get; private set; } = 0;
 
     public void UpArrowPressed() {
-        if (Selected - 1 >= 0) {
-            Actions[Selected].Selected = false;
-            Selected--;
-            Actions[Selected].Selected = true;
-        }
+        if (CurrentPosition - 1 < 0) return;
+
+        Actions[CurrentPosition].Selected = CurrentMode == Mode.NormalMode ?
+            false : CurrentPosition <= VisualModeStartPosition;
+
+        CurrentPosition--;
+        Actions[CurrentPosition].Selected = true;
     }
 
     public void DownArrowPressed() {
-        if (Selected + 1 < Actions.Count) {
-            Actions[Selected].Selected = false;
-            Selected++;
-            Actions[Selected].Selected = true;
+        if (CurrentPosition + 1 >= Actions.Count) return;
+
+        Actions[CurrentPosition].Selected = CurrentMode == Mode.NormalMode ?
+            false : CurrentPosition >= VisualModeStartPosition;
+
+        CurrentPosition++;
+        Actions[CurrentPosition].Selected = true;
+    }
+
+    public void VPressed() {
+        if (CurrentMode == Mode.NormalMode) {
+            CurrentMode = Mode.VisualMode;
+            VisualModeStartPosition = CurrentPosition;
+        } else {
+            for (int i = Math.Min(CurrentPosition, VisualModeStartPosition); i <= Math.Max(CurrentPosition, VisualModeStartPosition); i++) {
+                Actions[i].Selected = false;
+            }
+            Actions[CurrentPosition].Selected = true;
+            CurrentMode = Mode.NormalMode;
         }
     }
 }
