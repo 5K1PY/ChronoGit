3e31c95c414c2b2316dd125a2b6cc1c4ef4b6c86
Daniel Skýpala <daniel@honza.info>
2024-08-17 10:27:45 +0200

    Program: Add help

--- Program.cs
+++ Program.cs
@@ -10,8 +10,18 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) {
+        if (args.Length != 1 || args[0] == "-h" || args[0] == "--help") {
+            Console.WriteLine("ChronoGit - Visual sequence editor for interactive rebasing.");
+            Console.WriteLine("  Setting up as your sequence editor:");
+            Console.WriteLine("    git config --global sequence.editor ChronoGit");
+            Console.WriteLine("  Direct editing:");
+            Console.WriteLine("    ./ChronoGit <rebase-todo-file>");
+
+            return;
+        }
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
